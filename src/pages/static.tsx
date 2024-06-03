e6ea00bd60d9585a853cb4bc996c1db5fe4f8ba1
Fernando Barbosa <fernando_fe1@hotmail.com>
2024-06-03 15:24:41 -0300

    pagina estaticas

---
+++ src/pages/static.tsx
@@ -0,0 +1,64 @@
+import { GetServerSideProps, GetStaticProps, NextPage } from "next"
+import { ReactNode, useEffect, useState } from "react"
+import { Container, Col, Row } from "reactstrap"
+
+interface ApiResponse {
+    name: string
+    timestamp: Date
+}
+
+export const getStaticProps: GetStaticProps= async () => {
+    const staticData = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res =>res.json())
+    return {
+        props: {
+            staticData
+        }
+    }
+}
+
+const Static: NextPage = (props: {
+    children?: ReactNode
+    staticData?: ApiResponse
+}
+
+) => {
+    const [clientSideData, setClientSideData] = useState<ApiResponse>()
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch("/api/hello").then(res =>res.json())
+        setClientSideData(data)
+    }
+    return (
+        <Container tag="main">
+            <h1 className="my-5">
+                Como funcionam as renderizaçoes do Next.js
+            </h1>
+
+            <Row>
+                <Col>
+                    <h3>
+                    gerado estaticamente durante o build:
+                    </h3>
+                    <h2>
+                        {props.staticData?.timestamp.toString()}
+                    </h2>
+                </Col>
+                <Col>
+                    <h3>
+                    gerado no cliente:
+                    </h3>
+                    <h2>
+                        {clientSideData?.timestamp.toString()}
+                    </h2>
+                </Col>
+                
+            </Row>
+        </Container>
+    )
+}
+
+export default Static
